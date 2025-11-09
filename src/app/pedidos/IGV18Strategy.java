0a78e7579f4fae221e978669e8200f187f2d532f
lRen1505 <renso.escuadra@usil.pe>
2025-11-09 18:27:07 -0500

    Creamos el patron Strategy que implementa el calculo del IGV del 18%

--- src/app/pedidos/IGV18Strategy.java
+++ src/app/pedidos/IGV18Strategy.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package app.pedidos;
 
-/**
- *
- * @author yusuk
- */
-public class IGV18Strategy {
+
+public class IGV18Strategy implements ImpuestoStrategy {
     
+    private static final double IGV_RATE = 0.18;
+    
+    @Override
+    public double calcular(double subtotal) {
+        return subtotal * IGV_RATE;
+    }
+    
+   
+    @Override
+    public String getNombre() {
+        return "IGV 18%";
+    }
 }
