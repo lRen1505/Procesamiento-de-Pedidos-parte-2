3ed8b2fbf9c9dc06185b90acc34e7608d47cda1b
lRen1505 <renso.escuadra@usil.pe>
2025-11-09 18:23:51 -0500

    Creamos la interfaz que calcula impuesto basado en el subtotal y obtiene el nombre descriptivo de la estrategia

--- src/app/pedidos/ImpuestoStrategy.java
+++ src/app/pedidos/ImpuestoStrategy.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package app.pedidos;
 
-/**
- *
- * @author yusuk
- */
-public class ImpuestoStrategy {
-    
+
+public interface ImpuestoStrategy {
+    double calcular(double subtotal);
+    String getNombre();
 }
