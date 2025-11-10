71722acba2837027e08b83f99567c136a17a3792
lRen1505 <renso.escuadra@usil.pe>
2025-11-10 13:46:13 -0500

    Borré el main para hacerlo de nuevo

--- src/app/pedidos/Main.java
+++ src/app/pedidos/Main.java
@@ -6,22 +6,9 @@ public class Main {
 
 
     public static void main(String[] args) {
-      System.out.println("=== SISTEMA DE PROCESAMIENTO DE PEDIDOS ===\n");
+      System.out.println("=== SISTEMA DE PROCESAMIENTO DE PEDIDOS V.2 ===\n");
     
-   LegacyBillingSystem legacySystem = new LegacyBillingSystem();
-   FacturaService facturaService = new FacturaAdapter(legacySystem);
-   PedidoFacade pedidoFacade = new PedidoFacade(facturaService);
-   
-   System.out.println(" Pedido con stock suficiente");
-        String resultado1 = pedidoFacade.procesarPedido(
-            "Juan Velarde", 
-            "Laptop HP", 
-            2
-       );
-        System.out.println("Comprobante generado:");
-            System.out.println(resultado1);
-            
-            System.out.println("\n" + "=".repeat(60) + "\n");
+ 
             
     }
 }
